18b35b55c3c6a969548b8cc4e0340529019727a3
Coral Jimenez-Gudino <coraljimenez720@gmail.com>
2026-03-10 21:24:52 -0700

    Added a DotGraph class and GraphTest class

---
+++ CSE464PJ/src/DotGraph.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm.SingleSourcePaths;
+import org.jgrapht.alg.interfaces.StrongConnectivityAlgorithm;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.Graph;
+
+public class DotGraph {
+
+    //******* Feature 1 *******///
+    public void parseGraph(String filepath){
+        //Takes a DOT graph file and creates a directed graph
+    }
+
+    public String toString(){
+        //Returns number of nodes,label of nodes
+        //number of edges, nodes and the edge
+        return "Placeholder";
+    }
+
+    public void outputGraph(String filepath){
+        //Put the result of toString in a file???
+    }
+
+    //******* Feature 2 *******///
+    public void addNode(String label){
+        //Add a node and check for duplicate labels
+    }
+
+    public void addNodes(String label){
+        //Add a node and check for duplicate labels
+    }
+
+    //******* Feature 3 *******///
+    public void addEdge(String srcLabel, String dstLabel){
+        //Add an edge and check for duplicate labels
+    }
+
+    //******* Feature 4 *******///
+
+
+
+
+
+
+
+
+}
