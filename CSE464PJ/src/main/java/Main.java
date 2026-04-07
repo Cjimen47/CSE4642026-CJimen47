2d2d91508a55fae0d108098af66cede8ac482788
Coral Jimenez-Gudino <coraljimenez720@gmail.com>
2026-04-07 12:46:54 -0700

    Added some interaction to main class

--- CSE464PJ/src/main/java/Main.java
+++ CSE464PJ/src/main/java/Main.java
@@ -1,9 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello this is the main branch");
+        DotGraph userGraph = new DotGraph();
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Welcome to the Graph Manager");
+        System.out.println("Please enter the path of the file containing the graph");
+
+        String path = scn.nextLine();
+        userGraph.parseGraph(path);
+        System.out.println("Here is the graph you inputed: " );
+        System.out.println(userGraph.toString());
+
+
+
+
     }
 }
